db246a17a6c8d4f18bd444397aeb9c8b538d6715
Mani <manikandan.rajakannu@influx.co.in>
2017-08-04 19:48:07 +0530

    add playservice

--- app/src/main/java/com/mani/property/home/HomeActivity.java
+++ app/src/main/java/com/mani/property/home/HomeActivity.java
@@ -1,13 +1,14 @@
 package com.mani.property.home;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +19,15 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mani.property.R;
 import com.mani.property.common.Dialogbox;
 import com.mani.property.common.Localstorage;
@@ -35,7 +45,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class HomeActivity extends AppCompatActivity  {
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
     @BindView(R.id.drawer_layout)
     DrawerLayout drawerLayout;
     @BindView(R.id.ivMenu)
@@ -56,6 +66,8 @@ public class HomeActivity extends AppCompatActivity  {
     TextView tvMapview;
     @BindView(R.id.tvListview)
     TextView tvListview;
+    private MapView mMapView;
+    private GoogleMap googleMap;
     private boolean doubleBackToExitPressedOnce = false;
        private ArrayList<PropertyModel>arrProperty=new ArrayList<>();
     @Override
@@ -64,7 +76,28 @@ public class HomeActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setUpNavigationView();
-        Log.w("test", "onCreate: test terminal" );
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);*/
+        MapsInitializer.initialize(getApplicationContext());
+        mMapView = (MapView) findViewById(R.id.near_by_map);
+        mMapView.getMapAsync(this);
+       // mMapView.onResume();
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
+
+            }
+        });
+       // mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -158,6 +191,7 @@ public class HomeActivity extends AppCompatActivity  {
                     PropertyResp model = response.body();
                     if (model != null && model.getStatus() != null && model.getStatus().getId().equalsIgnoreCase("1")) {
                         arrProperty=model.getProperties();
+                        setMArkers(arrProperty);
                     } else {
                         Dialogbox.alerts(HomeActivity.this, model.getStatus().getDescription(), "2");
                     }
@@ -173,5 +207,92 @@ public class HomeActivity extends AppCompatActivity  {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void setMArkers(ArrayList<PropertyModel>locArr) {
+
+            for (int i = 0; i < locArr.size(); i++) {
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(new LatLng(Double.valueOf(locArr.get(i).getLatitude()), Double.valueOf(locArr.get(i).getLongitude()))).
+                        icon(BitmapDescriptorFactory.fromResource(R.mipmap.location_label_purple));
+               // Marker marker = googleMap.addMarker(markerOptions);
+              //  marker.showInfoWindow();
+
+            }
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Double.valueOf(locArr.get(0).getLatitude()), Double.valueOf(locArr.get(0).getLongitude())), 10));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(Double.valueOf(locArr.get(0).getLatitude()), Double.valueOf(locArr.get(0).getLongitude())))      // Sets the center of the map to location user
+                .zoom(10)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+          /*  googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+                @Override
+                public View getInfoWindow(Marker arg0) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker arg0) {
+
+
+                    View v = getActivity().getLayoutInflater().inflate(R.layout.window_info_layout, null);
+                    LatLng position = arg0.getPosition();
+
+                    TextView locname = (TextView) v.findViewById(R.id.loc_name);
+                    TextView locdist = (TextView) v.findViewById(R.id.loc_dist);
+                    TextView locadd = (TextView) v.findViewById(R.id.loc_address);
+                    ImageView marker_image = (ImageView) v.findViewById(R.id.marker_image);
+                    for (int i = 0; i < locArr.size(); i++) {
+                        if (position.latitude == locArr.get(i).getLatitude()) {
+                            locname.setText(locArr.get(i).getLocName());
+                            locdist.setText(locArr.get(i).getDistance()+" KM");
+                            locadd.setText(locArr.get(i).getLocAddress());
+                            Picasso.with(getActivity())
+                                    .load(locArr.get(i).getImage_URL())
+                                    .placeholder(R.mipmap.mytvs_logo)
+                                    .error(R.mipmap.mytvs_logo)
+                                    .into(marker_image);
+                            Service_center_contact_no_mobile = locArr.get(i).getMobile();
+                            Service_center_directions = locArr.get(i).getLatitude() + "," + locArr.get(i).getLongitude();
+                            Service_center_book_service = locArr.get(i).getID();
+                            Service_center_contact_no_lanline = locArr.get(i).getPhone();
+                            tinyDB.putString("Serive_center_ID", Service_center_book_service);
+
+                            if (!Service_center_contact_no_mobile.equalsIgnoreCase("") && !Service_center_directions.equalsIgnoreCase("") && !Service_center_book_service.equalsIgnoreCase("")) {
+                                mFabmennu.setVisibility(View.VISIBLE);
+                            } else {
+                                mFabmennu.setVisibility(View.GONE);
+                            }
+
+                        }
+                    }
+
+                    v.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    });
+                    return v;
+
+                }
+            });
+
+            googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                @Override
+                public void onInfoWindowClick(Marker marker) {
+                    NextScren();
+                }
+            });*/
+    }
+
+    @Override
+    public void onMapReady(GoogleMap mgoogleMap) {
+        googleMap = mgoogleMap;
     }
 }
